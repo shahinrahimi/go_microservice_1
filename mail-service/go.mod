45650e89d33e36f1a4e2e0e013a38f5999a3cdec
Shahin Rahimi <sh.rahimi.dev@gmail.com>
2025-02-26 22:13:41 +0330

    add mailer serveice almost done

--- mail-service/go.mod
+++
@@ -1,8 +0,0 @@
-module mail
-
-go 1.24.0
-
-require (
-	github.com/go-chi/chi/v5 v5.2.1 // indirect
-	github.com/go-chi/cors v1.2.1 // indirect
-)
---
+++ mailer-service/cmd/api/handlers.go
@@ -0,0 +1,39 @@
+package main
+
+import "net/http"
+
+func (app *Config) SendMail(w http.ResponseWriter, r *http.Request) {
+	type mailMessage struct {
+		From    string `json:"from"`
+		To      string `json:"to"`
+		Subject string `json:"subject"`
+		Message string `json:"message"`
+	}
+
+	var requestPayload mailMessage
+
+	if err := app.readJSON(w, r, &requestPayload); err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	msg := Message{
+		From:    requestPayload.From,
+		To:      requestPayload.To,
+		Subject: requestPayload.Subject,
+		Data:    requestPayload.Message,
+	}
+
+	if err := app.Mailer.SendSMTPMessage(msg); err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "send to " + requestPayload.To,
+	}
+
+	app.writeJSON(w, http.StatusAccepted, payload)
+
+}
